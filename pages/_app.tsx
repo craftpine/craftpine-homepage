a5ec1c67b8d208baad97fc6713e2d2347512156f
hoangdai98 <hoangdai9895@gmail.com>
2022-05-19 21:00:29 +0700

    fix: resolve wrong director

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Box, ChakraProvider, Container } from "@chakra-ui/react";
-import Head from "next/head";
-import Navbar from "../components/NavBar";
-import theme from "../libs/theme";
-import Fonts from "../components/Fonts";
-import { AnimatePresence } from "framer-motion";
-import Footer from "../components/Footer";
 import Layout from "../components/layouts/Layout";
 
 if (typeof window !== "undefined") {
